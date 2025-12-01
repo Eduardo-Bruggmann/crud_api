1a5fd2d652b648872f40223ee92d41b2777cf418
Blazz <143752142+Eduardo-Bruggmann@users.noreply.github.com>
2025-12-01 14:33:37 -0300

    Migrations fixed: identifiers from lowercase to uppercase

--- prisma/migrations/20251124182711_fix_category_id_to_optional_in_post/migration.sql
+++ prisma/migrations/20251124182711_fix_category_id_to_optional_in_post/migration.sql
@@ -1,11 +1,11 @@
 -- DropForeignKey
-ALTER TABLE `post` DROP FOREIGN KEY `Post_categoryId_fkey`;
+ALTER TABLE `Post` DROP FOREIGN KEY `Post_categoryId_fkey`;
 
 -- DropIndex
-DROP INDEX `Post_categoryId_fkey` ON `post`;
+DROP INDEX `Post_categoryId_fkey` ON `Post`;
 
 -- AlterTable
-ALTER TABLE `post` MODIFY `categoryId` INTEGER NULL;
+ALTER TABLE `Post` MODIFY `categoryId` INTEGER NULL;
 
 -- AddForeignKey
 ALTER TABLE `Post` ADD CONSTRAINT `Post_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
